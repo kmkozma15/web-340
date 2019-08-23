40607ee53cc4a1daa57213fc04e080ca6762bcf9
Keith Kozma <kmkozma15@msn.com>
2019-08-23 01:01:28 -0700

    Adding exercise 3.3

--- week-3/advanced-routing/kozma-exercise-3.3.js
+++ week-3/advanced-routing/kozma-exercise-3.3.js
@@ -1,3 +1,15 @@
+// Header file output
+var header = require("../../kozma-header.js")
+console.log(header.display("Keith" , "Kozma", "Assignment 3.3") + '\n')
+/*
+============================================
+; Title: Assignment 3.3
+; Author: Keith Kozma
+; Date: 23 August 2019
+; Description: This program creates a server and logs the product ID on the .ejs div container within the webpage.
+;===========================================
+*/
+//Creating the required library statements and assigning express to the app variable
 var express = require('express');
 var http = require('http');
 var path = require('path');
@@ -5,11 +17,13 @@ var logger = require('morgan');
 
 var app = express();
 
+//Sets the directory to locate the .ejs file
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 
+//This creates a request to listen for anything that comes after the /
 app.get('/:productId', function(req, res){
   var productId = parseInt(req.params.productId, 10);
 
@@ -18,6 +32,7 @@ app.get('/:productId', function(req, res){
   });
 });
 
+//Creates the server on port 3001
 http.createServer(app).listen(3001, function(){
   console.log('Application started and listening on port %s', 3001);
 });
