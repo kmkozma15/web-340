b41dfc0eda13634481434ee8783be2797c4258be
Keith Kozma <kmkozma15@msn.com>
2019-08-23 00:27:08 -0700

    Adding files for exercise 3.2

---
+++ week-3/morgan-logging/kozma-exercise-3.2.js
@@ -0,0 +1,26 @@
+
+//Creating required library statements and assigning express to variable
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
+
+var app = express();
+
+//Sets which directory to view the .ejs file
+app.set('views', path.resolve(__dirname,views));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+
+//Creates a get request to listen for any request with a /
+app.get('/', function(req, res){
+  res.render('index', {
+    message: 'Welcome to Exercise 3.2: The Morgan Logger Example!'
+  });
+});
+
+//Creates a server on port 3000
+http.createServer(app).listen(3000, function(){
+  console.log('Application started and listening on port %s', 3000);
+});
