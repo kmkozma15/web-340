045d6909568a9338571ec9f899d091ae1264d56e
Keith Kozma <kmkozma15@msn.com>
2019-09-06 22:06:15 -0700

    Adding exercise 5.2

---
+++ week-5/if-else-render/kozma-exercise-5.2.js
@@ -0,0 +1,30 @@
+// require
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+//app functions
+var app = express();
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//local composer array
+var composers = [
+  "Bach",
+  "Mozart",
+  "Beethoven",
+  "Verdi"
+];
+
+//routes
+app.get('/', function (req, res) {
+  res.render('index', {
+    names: composers
+  });
+});
+
+//create server
+http.createServer(app).listen(3000, function () {
+  console.log('Application has started and is listening on port 3000');
+
+});
